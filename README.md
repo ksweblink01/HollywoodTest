3ce50c8c098d5958403441fdabd1971e2347d7dd
ksweblink01 <49473687+ksweblink01@users.noreply.github.com>
2019-04-10 11:34:54 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# HollywoodTest
+# SampleLogin
+Simple design for admin screen and a login screen
+
+Tech used: HTML, CSS and Javascript
+Framework: Font Awesome
---
+++ js/script.js
@@ -0,0 +1,14 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+// $('.btn').on('click', function() {
+//     var $this = $(this);
+//   $this.button('loading');
+//     setTimeout(function() {
+//        $this.button('reset');
+//    }, 8000);
+// });
